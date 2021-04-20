5171460d1fa538c95ad3c2dcbaa3d175f301267a
cpelichek <cpelichek@gmail.com>
2021-04-20 10:26:29 -0300

    Add date-fns, a modern js date utility library for browser and Node.js; add header component with own style, using the style.module concept; add public svgs and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import { Header } from "../components/Header";
+
 export default function Home() {
   return (
     <>
+      <Header />
       <h1>Hello World from Next.js!</h1>
       <p>Lit off!🚀</p>
     </>
