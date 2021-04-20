4268e1712245b9f3375007ed949eb73865c6fad9
cpelichek <cpelichek@gmail.com>
2021-04-20 13:09:54 -0300

    Add API as a SPA, which is not visible to Search Engines because it runs on the browser engine; in our case, we will replace with a server side strategy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
-import { Header } from "../components/Header";
+import { useEffect } from "react";
 
 export default function Home() {
+  //API usando modelo SPA (Single Page Application)
+  useEffect(() => {
+    fetch("http://localhost:3333/episodes")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+  // Tem o problema de não indexar para os Search Engines, robots e crawlers, pois a forma como fizemos acima está rodando no js do browser, então não virá montado do servidor, logo o robot não enxerga
+
   return <h1>🛸</h1>;
 }
