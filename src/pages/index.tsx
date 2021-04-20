639191e91d8df881774368b1ba10ad88fb8e4536
cpelichek <cpelichek@gmail.com>
2021-04-20 11:05:18 -0300

    Add html and css for component Player, which like Header component is present in all pages, and for that reason we use it in _app.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,5 @@
 import { Header } from "../components/Header";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <h1>Hello World from Next.js!</h1>
-      <p>Lit off!🚀</p>
-    </>
-  );
+  return <h1>🛸</h1>;
 }
