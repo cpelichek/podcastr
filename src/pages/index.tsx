0bef81a6c45f9ff06f60a9a8d675dd1603c12403
cpelichek <cpelichek@gmail.com>
2021-04-21 10:26:17 -0300

    Add folder utils with function for formating and converting our duration from number to string; add formatting to our data when we get it, rather than when we render the component, because this way we don't re-render by accident; add type to all our data variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import { GetStaticProps } from "next";
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
 type Episode = {
   id: string;
   title: string;
-  members: string[];
-  // ...
+  thumbnail: string;
+  members: string;
+  publishedAt: string;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string;
 };
 
 type HomeProps = {
@@ -17,26 +25,43 @@ export default function Home(props: HomeProps) {
   return (
     <>
       <h1>🛸</h1>
-      {/* <p>{JSON.stringify(props.episodes)}</p> */}
+      <p>{JSON.stringify(props.episodes)}</p>
     </>
   );
 }
 
 //API usando modelo SSG (Static Site Generation) e tipagem do typescript
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await api.get("episodes", {
+  const { data } = await api.get("episodes", {
     params: {
       _limit: 12,
       _sort: "published_at",
       _order: "desc",
     },
   });
-  const data = await response.data;
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
-    revalidate: 60,
+    revalidate: 60 * 60 * 8,
   };
 };
