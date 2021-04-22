462ddeebf648b36bf5c7a18213a2c691c43f4ec3
cpelichek <cpelichek@gmail.com>
2021-04-22 17:59:13 -0300

    Add ref to <audio> and  add functionalities to manipulate it with side effects using useEffect

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useRef, useEffect } from "react";
 import Image from "next/image";
 import Slider from "rc-slider";
 
@@ -9,9 +9,31 @@ import { PlayerContext } from "../../contexts/PlayerContext";
 import styles from "./styles.module.scss";
 
 export function Player() {
-  const { episodeList, currentEpisodeIndex, isPlaying } = useContext(
-    PlayerContext
-  );
+  // Para conseguirmos manipular um elemento html de maneira imperativa, no js tradicional usaríamos document.getElementById("id").algumMetodoQueQueremosExecutar ou algo similar. No React, nós fazemos algo parecido com o uso de "Refs", uma referência para aquele elemento que queremos manipular. useRef() quando estamos tratando de criar referências para elementos html uma boa prática é sempre iniciar ela como null, e por estarmos usando typescript podemos tipar o useRef falando qual o tipo de elemento que vamos salvar/armazenar dentro do useRef, e quando estamos usando typescript com HTML todos os elementos HTML estão disponíveis de maneira global, ou seja toda a tipagem dos elementos estão disponíveis de forma global. Dessa forma, quando escrevemos useRef<HTMLAudioElement> , isso irá nos fornecer uma inteligência na hora que formos utilizar essa referência do audio (audioRef) para saber por exemplo que métodos tem lá dentro e o que podemos ou não podemos utilizar
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const {
+    episodeList,
+    currentEpisodeIndex,
+    isPlaying,
+    togglePlay,
+  } = useContext(PlayerContext);
+
+  // useEffect é uma função de dentro do React que usamos para fazer efeitos colaterais, ou seja, "quando alguma coisa muda eu executo algo"
+  useEffect(() => {
+    // Como nossa audioRef pode começar com null, então verificamos se ela tem um valor dentro dela. Toda Ref no React tem dentro dela uma única propriedade chamada .current , que é o valor da referência.
+    // Abaixo, se não tiver nenhum valor, não faça nada (se tentássemos executar alguma coisa daria erro)
+    if (!audioRef.current) {
+      return;
+    }
+
+    // Se isPlaying receber um novo valor e esse valor for truthy, pegamos podemos tocar ou pausar; esses métodos são do próprio HTMLAudioElement
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
 
   const episode = episodeList[currentEpisodeIndex];
 
@@ -57,8 +79,8 @@ export function Player() {
         </div>
 
         {/* Podemos colocar nossa tag <audio /> em qualquer lugar da nossa aplicação, ela ficará "invisível" no DOM, escolhemos aqui pois assim fica mais próxima dos botões que controlam o que estamos ouvindo */}
-        {/* Ao invés de um operador ternário, podemos fazer igual o exemplo abaixo quando queremos que a IIFE à direita aconteça apenas se a expressão à esquerda for truthy, ou seja, avaliado como true no processo de coerção do valor */}
-        {episode && <audio src={episode.url} autoPlay />}
+        {/* Ao invés de um operador ternário, podemos fazer igual o exemplo abaixo quando queremos que esta à direita aconteça apenas se a expressão à esquerda for truthy, ou seja, avaliado como true no processo de coerção do valor */}
+        {episode && <audio src={episode.url} ref={audioRef} autoPlay />}
 
         <div className={styles.buttons}>
           <button type="button" disabled={!episode}>
@@ -71,6 +93,7 @@ export function Player() {
             type="button"
             className={styles.playButton}
             disabled={!episode}
+            onClick={togglePlay}
           >
             {isPlaying ? (
               <img src="/pause.svg" alt="Pausar" />
--- src/contexts/PlayerContext.ts
+++ src/contexts/PlayerContext.ts
@@ -13,6 +13,7 @@ type PlayerContextData = {
   currentEpisodeIndex: number;
   isPlaying: boolean;
   play: (episode: Episode) => void;
+  togglePlay: () => void;
 };
 
 export const PlayerContext = createContext({} as PlayerContextData); // O valor default definido na chamada da função createContext() não é muito utilizado, serve mais para dar o formato que esperamos receber do value dos componentes envolvidos nesse contexto pelo "NomeDoContexto.Provider", exemplo: createContext({ episodeList: [], currentEpisodeIndex: 0, }) , porém nós usamos o createContext({} as PlayerContextData) que é um hack do typescript que "simula" ou "faz uma atribuição forçada" ao objeto, dizendo que esse {} tem a mesma estrutura que PlayerContextData, fazendo com que o PlayerContext tenha exatamente o formato do PlayerContextData. O bom disso é que quando formos importar os dados de dentro do player, quando fizermos uma desestruturação, exemplo const {  } = useContext(PlayerContext), nós podemos dentro do espaço em  {  } usar o atalho de teclado Ctrl+barraDeEspaço para enxergar o que é esperado dentro do PlayerContextData, no nosso caso episodeList e currentEpisodeIndex.
