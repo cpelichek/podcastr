e33721ca7b271080cd7a7726893219489ecf1228
cpelichek <cpelichek@gmail.com>
2021-04-22 16:20:56 -0300

    Add rc-slider package, and add better sliders to our player and further style it

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,5 +1,8 @@
 import { useContext } from "react";
 import Image from "next/image";
+import Slider from "rc-slider";
+
+import "rc-slider/assets/index.css";
 
 import { PlayerContext } from "../../contexts/PlayerContext";
 
@@ -34,29 +37,41 @@ export function Player() {
         </div>
       )}
 
-      <footer className={styles.empty}>
+      <footer className={!episode ? styles.empty : ""}>
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            {episode ? (
+              <Slider
+                trackStyle={{ backgroundColor: "#04d361" }}
+                railStyle={{ background: "#9f75ff" }}
+                handleStyle={{ borderColor: "#04d361", borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            )}
           </div>
           <span>00:00</span>
         </div>
 
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
-          <button type="button" className={styles.playButton}>
+          <button
+            type="button"
+            className={styles.playButton}
+            disabled={!episode}
+          >
             <img src="/play.svg" alt="Tocar" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próximo" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/repeat.svg" alt="Repetir" />
           </button>
         </div>
