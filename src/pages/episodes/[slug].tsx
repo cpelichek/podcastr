b4e8953ae9b2a696b29ebbaba7ea78724a88ce4e
cpelichek <cpelichek@gmail.com>
2021-04-23 17:59:53 -0300

    Add favicon and page title

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,6 +2,7 @@ import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
 // import { useRouter } from "next/router";   // ao invés disto estamos usando GetStaticPaths, já que estamos fazendo "páginas estáticas de forma dinâmica"
 import { GetStaticPaths, GetStaticProps } from "next";
 
@@ -38,6 +39,9 @@ export default function Episode({ episode }: EpisodeProps) {
 
   return (
     <div className={styles.episode}>
+      <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
       <div className={styles.thumbnailContainer}>
         <Link href="/">
           <button type="button">
